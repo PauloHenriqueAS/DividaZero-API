baee8880d0141afde40a8e2055f0651df5547028
PauloHenriqueAS <paulohalves_00@hotmail.com>
2023-09-12 20:21:57 -0300

    [REFACTOR] Refatorando models de banco

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ models.py
 This module contains all models that are being used in api.
 """
 
+from enum import Enum
 from typing import Optional, Text
 from pydantic import BaseModel
 
@@ -14,3 +15,22 @@ class User(BaseModel):
     id_user: Optional[int] = None
     email_user: str
     password_user: str
+
+## TODO FAZER as models nesse jeito, **lembrar que todo paramtro que não necessariamente venha na requisição na controller deve ser nulo**
+class Devedor(BaseModel):
+    """
+    Model of Devedor
+    """
+    id_devedor: Optional[int] = None
+    cpf: str
+    nome: Optional[str] = None
+    id_endereco: Optional[int] = None
+    estado_civil: Optional[str] = None
+    nacionalidade: Optional[str] = None
+
+## TODO FAZER UNS ENUM ASSIM
+ #   nacionalidade = Column(Enum('Argentino', 'Afegão', 'Alemã', 'Americano', 'Angolano', 'Antiguano', 'Argélia', 'Armeno', 'Australiano', 'Austríac', 'Bahamense', 'Bangladesh', 'Bechuano', 'Belga', 'Belizenho', 'Boliviano', 'Brasileiro', 'Camaronense', 'Canadense', 'Chileno', 'Chinês', 'Cingalês', 'Colombiano', 'Comorense', 'Costarriquenho', 'Croata', 'Cubano', 'Dinamarquês', 'Dominicana', 'Dominicano', 'Egípcio', 'Equatoriano', 'Escocês', 'Eslovaco', 'Esloveno', 'Espanhol', 'Filipina', 'Francês', 'Galês', 'Ganés', 'Granadino', 'Grego', 'Guatemalteco', 'Guianense', 'Guianês', 'Haitiano', 'Holandês', 'Hondurenho', 'Húngaro', 'Iemenita', 'Indiano', 'Indonésio', 'Inglês', 'Iraniano', 'Iraquiano', 'Irlandês', 'Israelita', 'Italiano', 'Jamaicano', 'Japonês', 'Líbio', 'Malaio', 'Marfinense', 'Marroquino', 'Mexicano', 'Moçambicano', 'Namibiana', 'Neozelandês', 'Nepalês', 'Nicaraguense', 'Nigeriano', 'Norte-coreano', 'Noruego', 'Omanense', 'Palestino', 'Panamenho', 'Paquistanês', 'Paraguaio', 'Peruano', 'Polonês', 'Portorriquenho', 'Portuguesa', 'Qatarense', 'Queniano', 'Romeno', 'Ruandês', 'Russo', 'Salvadorenho', 'Santa-lucense', 'Saudita', 'Somali', 'Sueco', 'Sul-Africano', 'Sul-africano', 'Sul-coreano', 'Surinamês', 'Suíço', 'São-cristovense', 'São-vicentino', 'Sérvio', 'Sírio', 'Tailandês', 'Timorense', 'Trindadense', 'Turco', 'Ucraniano', 'Ugandense', 'Uruguaio', 'Venezuelano', 'Vietnamita', 'Zimbabuense', 'Zimbabweano', 'barbadense', 'Árabe'), nullable=True, index=True)
+class NacionalidadeEnum(str, Enum):
+    BRASILEIRO = "Brasileiro"
+    ESTRANGEIRO = "Estrangeiro"
+    OUTRO = "Outro"
--- app/repositorys/model.py
+++ app/repositorys/model.py
@@ -4,7 +4,7 @@ app/Repositorys/model.py
 This module contains database models.
 """
 
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Boolean, Column, Date, Double, Enum, ForeignKey, Integer, String, Text
 from app.repositorys.configDb import Base
 
 class UserDb(Base):
@@ -17,3 +17,71 @@ class UserDb(Base):
     id_user = Column(Integer, primary_key=True, index=True)
     email_user = Column(String, index=True)
     password_user = Column(String, index=True)
+    tipo_user = Column(String, index=True)
+
+class EnderecoDb(Base):
+    __tablename__ = 'tb_endereco'
+    __table_args__ = {"schema": "saim"}
+
+    id_endereco = Column(Integer, primary_key=True, index=True)
+    lograduro = Column(String, nullable=False, index=True)
+    bairro = Column(String, nullable=False, index=True)
+    numero = Column(Integer, index=True) 
+    cep = Column(String,nullable=False, index=True)
+    complemento = Column(String, index=True)
+
+class DevedorDB(Base):
+    __tablename__ = 'tb_devedor'
+    __table_args__ = {"schema": "saim"}
+
+    id_devedor = Column(String, primary_key=True, index=True)
+    cpf = Column(String, index=True, nullable=False)
+    nome = Column(String, index=True, nullable=True)
+    id_endereco = Column(Integer, ForeignKey('EnderecoDb.id_endereco'), index=True, nullable=True)
+    estado_civil = Column(String, index=True, nullable=True)
+    nacionalidade = Column(String, index=True, nullable=True)
+
+class CredorDb(Base):
+    __tablename__ = 'tb_credor'
+    __table_args__ = {"schema": "saim"}
+
+    id_credor = Column(String, primary_key=True, index=True)
+    nome = Column(String, index=True)
+    cnpj = Column(String, index=True)
+    id_endereco = Column(Integer, ForeignKey('EnderecoDb.id_endereco'), index=True)
+
+class DividaDb(Base):
+    __tablename__ = 'tb_divida'
+    __table_args__ = {"schema": "saim"}
+
+    id_divida = Column(Integer, primary_key=True, index=True)
+    id_credor = Column(String, ForeignKey("CredorDb.id_credor"), nullable=False, index=True)
+    id_devedor = Column(String, ForeignKey("DevedorDb.id_devedor"), nullable=False, index=True)
+    num_contrato = Column(String, nullable=False, index=True)
+    termo_divida = Column(Text, nullable=False, index=True)
+    data_divida = Column(Date, nullable=False, index=True)
+    montante_valor = Column(Double, nullable=False, index=True)
+    montante_atrasado = Column(Double, nullable=False, index=True)
+    status = Column(String, nullable=False, index=True)
+
+class TermoDividaDb(Base):
+    __tablename__ = 'tb_termo_conf_divida'
+    __table_args__ = {"schema": "saim"}
+
+    id_termo = Column(Integer, primary_key=True, index=True)
+    id_divida = Column(Integer, ForeignKey("DividaDb.id_divida"), nullable=False, index=True)
+    num_parcela = Column(Integer, index=True)
+    termo_renegociacao = Column(Text, index=True)
+    data_renegociacao = Column(Date, index=True)
+    assinatura_devedor = Column(String, index=True, nullable=True)
+    assinatura_devedor = Column(String, index=True, nullable=True)
+
+class ParcelaDb(Base):
+    __tablename__ = 'tb_parcela'
+    __table_args__ = {"schema": "saim"}
+
+    id_parcela = Column(Integer, primary_key=True, index=True)
+    id_termo = Column(Integer, ForeignKey("TermoDividaDb.id_termo"), nullable=False, index=True)
+    valor_parcela = Column(Double, index=True)
+    data_vencimento = Column(Date, index=True)
+    parcela_paga = Column(Boolean, index=True, nullable=True)
