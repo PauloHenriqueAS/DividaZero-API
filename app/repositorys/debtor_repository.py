f1b18da142b425e8b97dfd10a1b6ec10fb502984
Lucas Gabriel <lucasgabriel.violeiro@gmail.com>
2023-09-16 17:31:41 -0300

    [CHORE]add services get repository

--- app/repositorys/debtor_repository.py
+++ app/repositorys/debtor_repository.py
@@ -4,15 +4,48 @@ app/Repositorys/debtor_repository.py
 This module contains debtor-methods.
 """
 
+from app.models import Credor
+from app.repositorys.model import CredorDb
+from .configDb import SessionLocal
+from sqlalchemy import func
+from sqlalchemy.exc import IdentifierError
+
 class DebtorRepository:
     """
     Debtor Repository
     """
-    def get_debtor_by_cnpj(self):
+    def get_debtor_by_cnpj(self, cnpj:str):
         """
         Get data address by cnpj
         """
-        return {"mensagem": "dados para dash estatisticas dos metodos de processamento de imagem"}
+        try:
+            db = SessionLocal()
+            credor = db.query(CredorDb).filter(CredorDb.cnpj == cnpj).first
+            db.close()
+
+            if credor is None:
+                return {"code": 302, "mensagem": "Usuário não cadastrado"}
+            else:
+                return {credor}
+        except IntegrityError as error:
+            return {"code": 404, "mensagem": f"Erro ao obter usuário. ERRO: {error}"}
+
+
+    def get_debtor_by_id(self, id:str):
+        """
+        Get data address by id
+        """
+        try:
+            db = SessionLocal()
+            credor = db.query(CredorDb).filter(CredorDb.id_credor == id).first
+            db.close()
+
+            if credor is None:
+                return {"code": 302, "mensagem": "Usuário não cadastrado"}
+            else:
+                return {credor}
+        except IntegrityError as error:
+            return {"code": 404, "mensagem": f"Erro ao obter usuário. ERRO: {error}"}
 
     def post_debtor(self):
         """
--- app/services/term_service.py
+++ app/services/term_service.py
@@ -12,6 +12,19 @@ from app.repositorys.debt_repository import debt_repository
 from app.repositorys.debtor_repository import debtor_repository
 from app.repositorys.address_repository import address_repository
 from app.models import Credor, Devedor, Divida, Endereco, TermoDivida, Parcela, Parcelamento
+from datetime import date
+
+def adicionar_meses(data, n):
+    # Adicionar n meses à data fornecida
+    novo_ano = data.year + (data.month + n - 1) // 12
+    novo_mes = (data.month + n - 1) % 12 + 1
+
+    # Garantir que o dia não ultrapasse o último dia do novo mês
+    ultimo_dia_do_mes = (data.replace(year=novo_ano, month=novo_mes, day=1) + timedelta(days=31)).replace(day=1) - timedelta(days=1)
+    novo_dia = min(data.day, ultimo_dia_do_mes.day)
+
+    nova_data = data.replace(year=novo_ano, month=novo_mes, day=novo_dia)
+    return nova_data
 
 class TermService:
     """
@@ -23,7 +36,7 @@ class TermService:
         """
         return term_repository.get_term_by_id()
 
-    def post_term(self, termo:TermoDivida, parcelas:Parcelamento):
+    def post_term(self, termo:TermoDivida, parcelas:[Parcelamento]):
         """
         Insert new data term
         """
@@ -32,8 +45,17 @@ class TermService:
         credor = debtor_repository.get_debtor_by_id(divida.id_credor)
         end_cred = address_repository.get_address_by_id(credor.id_endereco)
         end_dev = address_repository.get_address_by_id(devedor.id_endereco)
+        
+        data = data.today()
+        
+        for parcela in parcelas:
+            for i in range(parcela.qtd):
+                parcelaDb = Parcela(valor_parcela=parcela.valor,
+                                    data_vencimento = adicionar_meses(data,i+1),
+                                    parcela_paga=False)
 
         render(devedor,credor, divida, end_dev, end_cred,termo)
+
 
         # TODO: assinatura
 
